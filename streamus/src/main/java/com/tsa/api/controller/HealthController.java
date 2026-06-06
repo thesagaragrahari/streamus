1b41b0c1fc306bf12b83bfcf5cb1f476bbc311fe
sagar agrahari <138549469+thesagaragrahari@users.noreply.github.com>
2026-06-06 21:31:33 +0000

    some chngs with java 21

---
+++ streamus/src/main/java/com/tsa/api/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.streamus.api.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "StreamUs Running";
+    }
+
+}
